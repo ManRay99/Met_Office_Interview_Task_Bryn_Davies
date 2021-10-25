031ec5a8ed45fe0979d52de18085c38b2af9ec8b
Bryn Davies <h09bryndavies@gmail.com>
2021-10-25 18:01:06 +0100

    Part 2 complete

--- Main.py
+++ Main.py
@@ -1,20 +1,25 @@
 import os
 
 def calc_shielding(dimensions):
-    dimensions.sort()
     return (3*dimensions[0]*dimensions[1]) + (2*dimensions[1]*dimensions[2]) + (2*dimensions[0]*dimensions[2])
+
+def calc_wiring(dimesions):
+    return (dimensions[0]*dimensions[1]*dimesions[2]) + 2*(dimesions[0]+dimesions[1])
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "examples");
 dimensions = [];
-print(filename)
 for examplefile in os.listdir(filename):
-    totalshielding = 0
+    totalshielding = 0;
+    totalwiring = 0;
     with open(filename+ "\\" + examplefile) as file:
         for line in file:
-            dimensions = list(map(int, line.split("x")))
-            totalshielding += calc_shielding(dimensions)
+            dimensions = list(map(int, line.split("x")));
+            dimensions.sort();
+            totalshielding += calc_shielding(dimensions);
+            totalwiring += calc_wiring(dimensions);
         print(examplefile + " total shielding required = " + str(totalshielding));
+        print(examplefile + " total wiring required = " + str(totalwiring));
 
 
 
