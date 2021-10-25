e45c4f9322064b4ec5736047b050e7282f49d6b9
Bryn Davies <h09bryndavies@gmail.com>
2021-10-25 17:54:46 +0100

    completed Part One

---
+++ Main.py
@@ -0,0 +1,24 @@
+import os
+
+def calc_shielding(dimensions):
+    dimensions.sort()
+    return (3*dimensions[0]*dimensions[1]) + (2*dimensions[1]*dimensions[2]) + (2*dimensions[0]*dimensions[2])
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, "examples");
+dimensions = [];
+print(filename)
+for examplefile in os.listdir(filename):
+    totalshielding = 0
+    with open(filename+ "\\" + examplefile) as file:
+        for line in file:
+            dimensions = list(map(int, line.split("x")))
+            print(dimensions)
+            totalshielding += calc_shielding(dimensions)
+        print("total shielding required = " + str(totalshielding));
+
+
+
+
+
+
